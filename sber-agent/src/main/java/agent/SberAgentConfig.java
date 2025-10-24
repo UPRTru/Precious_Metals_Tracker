5f18a496489784adc4dcbc3c12e343fb39db886a
a.chichulin <a.chichulin@infomaximum.com>
2025-10-24 12:13:38 +0300

    currency sber agent 24.10.2025

--- sber-agent/src/main/java/agent/SberAgentConfig.java
+++ sber-agent/src/main/java/agent/SberAgentConfig.java
@@ -4,18 +4,18 @@ public enum SberAgentConfig {
     CURRENCY_URL("https://www.sberbank.ru/ru/quotes/currencies?tab=sbol"),
     METAL_URL("https://www.sberbank.ru/retail/ru/quotes/metalbeznal?tab=online"),
 
-    CURRENCY_WEB_ELEMENT("//div[contains(@class, 'rates-form-new-table-row')]"
+    CURRENCY_WEB_ELEMENT("//div[starts-with(@class, 'TabContainer') and not(contains(substring-after(@class, 'TabContainer'), ' '))]"
+            + "//div[contains(@class, 'rates-form-new-table-row')]"
             + "[contains(., '%s') or contains(., '%s')]"
             + "//div[contains(@class, 'rates-form-new-table-row__col-wrap')]"
             + "//div[%s]"
             + "//div[contains(@class, 'dk-sbol-text') and contains(text(), '₽')]"),
-    METAL_WEB_ELEMENT("//div[contains(@class, 'rfn-table-currency__iso') and text()='"
-            + "%s"
-            + "']/ancestor::div[contains(@class, 'rfn-table-row')]" +
+    METAL_WEB_ELEMENT("//div[contains(@class, 'rfn-table-currency__iso') " +
+            "and text()='%s']" +
+            "/ancestor::div[contains(@class, 'rfn-table-row')]" +
             "//div[contains(@class, 'rfn-table-row__price_main')]" +
-            "//div[contains(@class, 'rfn-table-row__col')]["
-            + "%s"
-            + "]"),
+            "//div[contains(@class, 'rfn-table-row__col')]" +
+            "[%s]"),
 
     CURRENCY_INDEX_BUY("1"),
     CURRENCY_INDEX_SELL("2"),
